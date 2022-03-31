3a28a13541ea02749c70f89f3b72a800005a5e49
augustojs-tagchat <augusto.souza@tagchat.com.br>
2022-03-31 10:18:45 -0300

    feat: add pagination to properties route

--- src/property/property.service.ts
+++ src/property/property.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Property, PropertyDocument } from './schema/property.schema';
+import { PaginationDTO } from './dto/pagination.dto';
+import { any, number } from 'joi';
 
 @Injectable()
 export class PropertyService {
@@ -10,7 +12,49 @@ export class PropertyService {
     private readonly propertyModel: Model<PropertyDocument>,
   ) {}
 
-  public async findAll() {
-    return await this.propertyModel.find();
+  public async findAll(pagination: PaginationDTO) {
+    const page = parseInt(pagination.page, 10) || 1;
+    const limit = parseInt(pagination.limit, 10) || 25;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const total = await this.propertyModel.countDocuments();
+
+    let query = this.propertyModel.find();
+
+    query = query.skip(startIndex).limit(limit);
+
+    const properties = await query;
+
+    // Pagination result
+    const paginatedData = {
+      next: {
+        page: 1,
+        limit: 1,
+      },
+      prev: {
+        page: 1,
+        limit: 1,
+      },
+    };
+
+    if (endIndex < total) {
+      paginatedData.next = {
+        page: page + 1,
+        limit,
+      };
+    }
+
+    if (startIndex > 0) {
+      paginatedData.prev = {
+        page: page - 1,
+        limit,
+      };
+    }
+
+    return {
+      data: properties,
+      count: properties.length,
+      paginatedData,
+    };
   }
 }
